bee60d50f2c20377283d53a03f3644d01a8d6e5d
abhilash-sunkara <52720913+abhilash-sunkara@users.noreply.github.com>
2024-11-17 21:38:04 -0600

    more fixes with escaping and animations

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 
 import {Montserrat, Quantico} from 'next/font/google'
 import HeaderBar from "../components/headerbar";
+import { motion } from "framer-motion";
+import { slideFromBottom } from "../components/animations";
 
 const montserrat = Montserrat({
   weight: ["400", "700"], // Specify the weights you need
@@ -22,16 +24,17 @@ const quantico = Quantico({
 export default function Home() {
 
   const BlogPosts:BlogElementType[] = [
-    {title : "Kickoff", date : "9/7/2024", imgpath : "/blog_posts/kickoff.png", text : "Kicking off our rookie season in FTC has been both thrilling and challenging. We&apos;re diving headfirst into the game rules, brainstorming robot designs, and figuring out the basics of coding and building. Every day feels like a new lesson in teamwork and problem-solving, and we&apos;re excited to see how far we can push ourselves. This season, we&apos;re determined to grow together and make our mark in the competition."},
-    {title : "Ready for Tele-op and Autonomous", date : "9/24/24", imgpath : "/blog_posts/fieldsetup.png", text : "Setting up the field for the first time felt surreal – seeing it all laid out made the competition feel real. With the field ready, we&apos;ve started writing our first lines of code, working on getting the robot&apos;s movements just right. Learning the basics of programming each motor and sensor is a challenge, but we&apos;re excited to bring our ideas to life and see our robot in action!"},
-    {title : "Meet 1 Finished", date : "11/2/2024", imgpath : "/blog_posts/meet1bot.png", text : "We just finished our first league meet, and we couldn&apos;t be prouder – we placed 4th in the league! Competing for the first time was intense, but it showed us how much we&apos;ve grown as a team. This experience has only motivated us more, and we&apos;re excited to keep improving and pushing our limits for the next competition."}
+    {title : "Kickoff", date : "9/7/2024", imgpath : "/blog_posts/kickoff.png", text : "Kicking off our rookie season in FTC has been both thrilling and challenging. We're diving headfirst into the game rules, brainstorming robot designs, and figuring out the basics of coding and building. Every day feels like a new lesson in teamwork and problem-solving, and we're excited to see how far we can push ourselves. This season, we're determined to grow together and make our mark in the competition."},
+    {title : "Ready for Tele-op and Autonomous", date : "9/24/24", imgpath : "/blog_posts/fieldsetup.png", text : "Setting up the field for the first time felt surreal – seeing it all laid out made the competition feel real. With the field ready, we've started writing our first lines of code, working on getting the robot's movements just right. Learning the basics of programming each motor and sensor is a challenge, but we're excited to bring our ideas to life and see our robot in action!"},
+    {title : "Meet 1 Finished", date : "11/2/2024", imgpath : "/blog_posts/meet1bot.png", text : "We just finished our first league meet, and we couldn't be prouder – we placed 4th in the league! Competing for the first time was intense, but it showed us how much we've grown as a team. This experience has only motivated us more, and we're excited to keep improving and pushing our limits for the next competition."}
   ]
 
   return (
     <main className={`flex min-h-screen flex-col items-start justify-start p-10 bg-emerald-50 ${montserrat.variable} ${quantico.variable}`}>
         
         <HeaderBar></HeaderBar>
-        
+        <motion.div variants = {slideFromBottom} initial = "hidden"
+         animate = "active">
         <div className="w-full">
         
         {BlogPosts.reverse().map((item, index) => (
@@ -42,6 +45,7 @@ export default function Home() {
         
 
         </div>
+        </motion.div>
     </main>
   );
 }
