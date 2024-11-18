e30c3f938dbdafc5521b607cc12d30701deb9853
abhilash-sunkara <52720913+abhilash-sunkara@users.noreply.github.com>
2024-11-17 21:07:45 -0600

    robot gallery updates

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -27,7 +27,15 @@ const quantico = Quantico({
 
 export default function Home() {
 
+  const GalleryPosts: GalleryElementType[] = [
+    {description : "First Prototype of Intake", imgpath : "/robot_gallery/prototypeintake.png"},
     
+    {description : "Final Version of Intake", imgpath : "/robot_gallery/intake.png"},
+    {description : "Steel Plates for Deposit", imgpath : "/robot_gallery/steelplates.png"},
+    {description : "Drivetrain Assembly", imgpath : "/robot_gallery/drivetrain (2).png"},
+    {description : "Robot Assembly", imgpath : "/robot_gallery/assemblydone.png"},
+    {description : "Robot Wiring", imgpath : "/robot_gallery/wiringdone.png"},
+  ]
 
     
   return (
@@ -37,12 +45,9 @@ export default function Home() {
         <motion.div variants = {slideFromBottom} initial = "hidden"
          animate = "active">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 py-8 w-full">
-            <GalleryElement text="Robot after Meet 1" imgpath="/meet1bot.png" description="This is our robot that performed at our first competition as a team" />
-            <GalleryElement text="Robot after Meet 2" imgpath="/meet1bot.png" description="This is our updated robot for the second meet" />
-            <GalleryElement text="Robot after Meet 3" imgpath="/meet1bot.png" description="Robot design after third meet" />
-            <GalleryElement text="Robot Final" imgpath="/meet1bot.png" description="Our robot for the final competition" />
-            <GalleryElement text="Team Meeting" imgpath="/meet1bot.png" description="Team working together" />
-            <GalleryElement text="On the Field" imgpath="/meet1bot.png" description="Robot on the field in action" />
+            {GalleryPosts.map((item, index) => (
+              <GalleryElement description={item.description} imgpath={item.imgpath} key = {index}></GalleryElement>
+            ))}
         </div>
         </motion.div>
         
@@ -56,19 +61,18 @@ export default function Home() {
 }
 
 type GalleryElementType = {
-    text : string,
     imgpath : string,
     description : string,
 }
 
 
 
-const GalleryElement: React.FC<GalleryElementType> = ({ text, imgpath, description }) => {
+const GalleryElement: React.FC<GalleryElementType> = ({imgpath, description }) => {
     return (
       <div className="relative group">
         <div className="flex flex-col bg-emerald-200 gap-4 transition-all duration-300 ease-in-out group-hover:blur-sm">
           <Image src={imgpath} width={400} height={200} style={{ borderRadius: 4 }} alt={""} />
-          <p className="text-slate-950 text-xl text-center font-bold font-montserrat">{text}</p>
+          
         </div>
 
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
